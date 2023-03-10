3dd4fe40253bc9baf7959436311811b003475d2e
MUKANTWARI Françoise <f.mukantwari@alustudent.com>
2023-03-10 11:55:17 +0200

    Create firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import "./splash.dart";
 import './Homepage.dart';
 import 'Feedpage.dart';
 
-void main() {
-  runApp(const MyApp());
+// void main() {
+//   runApp(const MyApp());
+// }
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 }
 
 class MyApp extends StatelessWidget {
